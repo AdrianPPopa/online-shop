945806a88b091257a8ea904295d1029d6a1ff552
Adi <popaadrianvd@gmail.com>
2019-09-25 21:30:55 +0300

    added cart service (studied one to one relationship)

--- src/test/java/org/fasttrackit/onlineshop/CustomerServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/CustomerServiceIntegrationTests.java
@@ -1,18 +1,13 @@
 package org.fasttrackit.onlineshop;
 
-import org.fasttrackit.onlineshop.domain.Customer;
+
 import org.fasttrackit.onlineshop.service.CustomerService;
-import org.fasttrackit.onlineshop.transfer.customer.SaveCustomerRequest;
+import org.fasttrackit.onlineshop.steps.CustomerSteps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.hamcrest.number.OrderingComparison.greaterThan;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,18 +16,12 @@ public class CustomerServiceIntegrationTests {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private CustomerSteps customerSteps;
+
     @Test
     public void testCreateCustomer_whenValidRequest_thanReturnCreatedCustomer() {
-        SaveCustomerRequest request = new SaveCustomerRequest();
-        request.setFirstName("CustomerFirstName");
-        request.setLastName("CustomerLastName");
-
-        Customer customer = customerService.createCustomer(request);
-        assertThat(customer, notNullValue());
-        assertThat(customer.getId(), notNullValue());
-        assertThat(customer.getId(), greaterThan(0L));
-        assertThat(customer.getFirstName(),is(request.getFirstName()));
-        assertThat(customer.getLastName(),is(request.getLastName()));
+        customerSteps.createCustomer();
     }
 
 }
